a2ec89421fe94651612fc5345e7393d622b74c82
Roman Korsun <korsunello@gmail.com>
2024-08-30 13:13:35 +0300

    Add files via upload

--- Core/Src/ingition.c
+++ Core/Src/ingition.c
@@ -7,37 +7,38 @@
 #include "ignition.h"
 
 uint16_t map_rpm[15] = {600, 800, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 7000, 8000};
-uint16_t map_angle[15] = {0, 2, 3, 7, 12, 17, 21, 26, 30, 33, 35, 37, 40, 42, 45};
+uint16_t map_angle[15] = {0, 2, 5, 7, 12, 17, 21, 26, 30, 33, 35, 37, 40, 42, 45};
 
-int rpm_conversion(int timer_counter){
-	int rpm=0;
+uint32_t calculate_rpm(uint32_t timer_counter){
+	uint32_t rpm=0;
+	if(timer_counter <= 10000){
 	rpm = timer_counter*6;
 	rpm = 64000/rpm;
 	rpm = rpm*60;
 	return rpm;
-}
-
-int set_delay_by(int rpm){
-	float angle_delay= 0, rpm_delay =0, angle= 0;
-
-	if(rpm >= 600){
-		for(int i =0; map_rpm[i]<rpm; i++){
-			angle = map_angle[i];
-		}
-		angle_delay= rpm/60;
-		rpm_delay= 64000/angle_delay; //1280 3000 25%
-		angle_delay = rpm_delay/360;
-		angle= angle*angle_delay;
-		angle_delay = rpm_delay -angle;
-		angle_delay = angle_delay - IGNITE_MS;
-
 	} else {
-		angle_delay= 130;
+		return 0;
 	}
-
-	int pizdos = angle_delay;
-	return pizdos;
 }
+
+uint32_t set_delay_by_rpm(uint32_t rpm){
+
+	uint32_t angle_dl=0, rpm_delay =0, angle= 0;
+	float f_temp=0;
+	for(int i =0; map_rpm[i]<=rpm; i++){
+		angle = map_angle[i];
+	}
+	angle_dl= rpm/60;
+	rpm_delay= 64000/angle_dl;
+	f_temp = angle_dl/360;
+	f_temp = angle*f_temp;
+	angle_dl = rpm_delay -f_temp;
+	angle_dl = angle_dl-128;
+	return angle_dl;
+}
+
+
+
 
 
 
